eba87fcb1b99fd24cc65ba472b6b093a39549512
Leonardo Ivo Neves da Silva <iblue.lsilva@terceiros.mag.com.br>
2021-02-24 22:56:10 -0300

    correção do arquivo autoloader.php, responsavel pela localização das classes do projeto

--- FG/StartLoader/autoloader.php
+++ FG/StartLoader/autoloader.php
@@ -6,7 +6,11 @@ function myAutoLoader($class)
 
     // Diretorio aonde ficam as bibliotecas
     $base_dir = __DIR__ . '/';
+     
+    $pos = strripos($base_dir,  "StartLoader");
 
+    
+    $caminho=  substr($base_dir,0, $pos);
     // Verifica se a classe chamada usa o prefixo
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
@@ -19,7 +23,7 @@ function myAutoLoader($class)
 
     // Troca os separadores de namespace por separadores de diretorio
     // e adiciona o .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.class.php';
+    $file = $caminho . str_replace('\\', '/', $relative_class) . '.class.php';
 
     // Verifica se o arquivo existe, se existir então inclui ele 
     if (is_file($file)) {
