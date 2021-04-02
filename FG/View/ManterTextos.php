09180f62344ce209124dfcb222394c0a4b95ef31
leonardo.ivo <leonardo.ivo22@gmail.com>
2021-04-02 18:57:59 -0300

    Continuação da expansão

--- FG/View/ManterTextos.php
+++ FG/View/ManterTextos.php
@@ -12,5 +12,14 @@ $TextoJornalisticoL = new TextoJornalisticoLO();
 
 //Funções principais
 $texto = new TextoJornalistico();
+foreach ($TextoJornalisticoL->getTextoJornalistico() as $textoJor){
+    echo $textoJor->titulo."<br/>";
+    echo $textoJor->subtitulo."<br/>";
+    echo $$textoJor->autor."<br/>";
+    echo $textoJor->datapublicacao."<br/>";
+
+}
+
+
 
 ?>
