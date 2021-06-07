8707d5656c4957b7b2408c0f5acbf01754960ff1
leonardo.ivo <leonardo.ivo22@gmail.com>
2021-06-07 03:44:03 -0300

    Construção da página de cadastramento de seções e correções na página de manutenção de textos - acertos adcionais

--- FG/LO/AcervoLO.class.php
+++ FG/LO/AcervoLO.class.php
@@ -4,29 +4,29 @@ namespace FG\LO
 use \ArrayObject;
 use FG\DTO\AcervoDTO;
 class AcervoLO{
-private $Acervo;
+private $acervo;
 
-function  __construct()
-{
-    $this->Acervo= new ArrayObject();
-}
-public function add(AcervoDTO $acervo)
+    function  __construct()
     {
-        //$this->Acervo->offsetSet($acervo->getTitulo(),$acervo); //Função porfora77
-        $this->Acervo->append($acervo); //adiciona um indice automatico
+      $this->acervo= new ArrayObject();
+    }
+    public function add(AcervoDTO $acervo)
+    {
+        //$this->acervo->offsetSet($acervo->getTitulo(),$acervo); //Função porfora77
+        $this->acervo->append($acervo); //adiciona um indice automatico
     }
     public function getAcervo(){
 
-        return $this->Acervo;
+        return $this->acervo;
     }
     public function del(AcervoDTO $acervo)
     {
-        $this->Acervo->offsetUnset($acervo);
+        $this->acervo->offsetUnset($acervo);
     }
 
     public function find(AcervoDTO $acervo)
     {
-        return $this->Acervo->offsetExists($acervo);
+        return $this->acervo->offsetExists($acervo);
     }
 
 }
--- FG/View/CriarAcervo.php
+++ FG/View/CriarAcervo.php
@@ -2,6 +2,49 @@
 use FG\DTO\AcervoDTO;
 use FG\LO\AcervoLO;
 use FG\BL\ManterAcervo;
-require 'StartLoader/autoloader.php';
+require '../StartLoader/autoloader.php';
+//Instâncias
+$Acervo = new ManterAcervo();
+
+//DTOs
+$AcervoDT = new AcervoDTO();
+//LO
+$lAcervoLO = new AcervoLO();
+//uso
+
+$lAcervoLO = $Acervo->ListarAcervo();
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<table>
+<?
+	foreach ($lAcervoLO->getAcervo() as $Acervotxt){
+		echo "<tr><td>".$Acervotxt->idAcervo."</td><td>.$Acervotxt->nome_acervo.</td></tr>";
+
+}
+?>
+</table>
+<form action="ManterAcervo.php" method="post">
+Nome da seção: <input type="text" name="Acervo">
+<input type="submit"  value="enviar">
+</form>
+
+</body>
+</html>
+<?
+$AcervoDT->nomeAcervo=isset($_POST['Acervo'])?$_POST['Acervo']:"";
+$Acervo->CriarAcervo($AcervoDT->nomeAcervo);
+?>
+
+
+
+
+
 
 ?>
