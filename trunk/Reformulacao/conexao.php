55ee7c98b3407a54db45c791ed7b0e547444f13e
leonardo.ivo <leonardo.ivo22@gmail.com>
2022-01-02 19:41:10 -0300

    retirada de pastas não utilizadas 5

--- trunk/Reformulacao/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$database="blog";
-//$tabela="usuarios";
-$login_db="root";
-$senha_db="784512";
-$link=mysqli_connect($host,$login_db,$senha_db);
-$db=mysqli_select_db($link,$database);
-?>
